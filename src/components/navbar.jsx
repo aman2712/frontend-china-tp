5ce68a3b76aff82d1ef131f6e25b8fdfdd8eaffc
aman <khanamankhan1973@gmail.com>
2024-08-18 10:21:10 +0530

    fixed small bug

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="absolute top-0 w-full sm:px-32 px-2">
       <div className="flex w-full items-center justify-between border-b border-gray-400 py-6">
       <h1 className="font-lato text-white text-3xl font-black hidden sm:block">Wonderful Lyon.</h1>
-      <ul className="flex items-center gap-7 sm:text-lg sm:mx-0 mx-auto">
+      <ul className="flex items-center sm:gap-7 gap-5 w-96 sm:text-lg sm:mx-0 mx-auto">
         <li>
           <a href="#" className="text-gray-200">&nbsp;Home&nbsp;</a>
         </li>
